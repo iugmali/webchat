8d4c5efc86a20b55dd36238f3efcb792c69ba25b
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2024-03-29 00:10:08 -0300

    update profanity

--- src/services/profanity.spec.ts
+++ src/services/profanity.spec.ts
@@ -11,13 +11,13 @@ describe("Profanity", () => {
     const badWord = badWords[Math.floor(Math.random() * badWords.length)];
     const censored = profanity.isProfane(badWord);
     expect(censored).toBe(true);
-    const expectedCensored = '*'.repeat(badWord.length);
+    const expectedCensored = badWord[0] + '*'.repeat(badWord.length - 1);
     expect(profanity.censor(badWord)).toBe(expectedCensored);
   });
   it("should maintain not profane words in a sentence", () => {
     const badWord = badWords[Math.floor(Math.random() * badWords.length)];
     const phrase = `This is a phrase with a ${badWord} in it`;
-    const expectedCensoredWord = '*'.repeat(badWord.length);
+    const expectedCensoredWord = badWord[0] + '*'.repeat(badWord.length - 1);
     const expectedCensoredPhrase = `This is a phrase with a ${expectedCensoredWord} in it`;
     expect(profanity.censor(phrase)).toBe(expectedCensoredPhrase);
   });
